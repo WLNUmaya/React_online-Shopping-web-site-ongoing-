59b431ca1058a18b00f949b0cb7236e4e8fb978c
WLNUmaya <158131901+WLNUmaya@users.noreply.github.com>
2024-03-25 21:58:03 +0530

    react

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+
+export const Shop = () => {
+  return (
+    <div>
+
+    <Hero/>
+
+
+    </div>
+  )
+}
+export default Shop
